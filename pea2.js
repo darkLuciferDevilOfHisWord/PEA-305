c12dc13dcb47a348d4153adf66e5f5971cdd2731
darkLuciferDevilOfHisWord <86288159+darkLuciferDevilOfHisWord@users.noreply.github.com>
2021-12-19 21:40:38 +0530

    Add files via upload

---
+++ pea2.js
@@ -0,0 +1,227 @@
+
+let img1_1 = ["a1","b1","c1","d1","e1","f1","g1","h1","i1","j1","k1","l1","m1","n1","o1","p1","q1","r1","s1","t1"];
+let img1_2 = ["u1","v1","w1","x1","y1","z1","za1"];
+let img1_3 = ["zb1"];
+
+function fun1_1(){
+    mainBox.innerHTML = "";
+    img1_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun1_2(){
+    mainBox.innerHTML = "";
+    img1_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun1_3(){
+    mainBox.innerHTML = "";
+    img1_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+let img2_1 = ["a2","b2","c2","d2",];
+let img2_2 = ["e2","f2","g2","h2","i2","j2","k2",];
+let img2_3 = ["l2","m2","n2","o2",];
+
+function fun2_1(){
+    mainBox.innerHTML = "";
+    img2_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun2_2(){
+    mainBox.innerHTML = "";
+    img2_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun2_3(){
+    mainBox.innerHTML = "";
+    img2_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+let img3_1 = ["a3","b3","c3","d3","e3","f3","g3"];
+let img3_2 = ["h3","i3","j3","k3","l3","m3","n3","o3"];
+let img3_3 = ["p3","q3","r3"];
+
+function fun3_1(){
+    mainBox.innerHTML = "";
+    img3_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun3_2(){
+    mainBox.innerHTML = "";
+    img3_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun3_3(){
+    mainBox.innerHTML = "";
+    img3_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+let img4_1 = ["a4","b4","c4","d4","e4","f4","g4","h4","i4"];
+let img4_2 = ["j4","k4","l4","m4","n4","o4","p4","q4","r4","s4","t4","u4","v4"];
+let img4_3 = ["w4","x4","y4","z4",];
+let img4_4 = ["za4","zb4","zc4","zd4","ze4","zf4","zg4"];
+
+function fun4_1(){
+    mainBox.innerHTML = "";
+    img4_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun4_2(){
+    mainBox.innerHTML = "";
+    img4_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun4_3(){
+    mainBox.innerHTML = "";
+    img4_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun4_4(){
+    mainBox.innerHTML = "";
+    img4_4.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+
+let img5_1 = ["a5","b5","c5","d5","e5","f5","g5","h5","i5","j5","k5","l5","m5","n5","o5","p5","q5","r5"];
+let img5_2 = ["s5","t5","u5","v5","w5","x5","y5","z5","za5","zb5","zc5","zd5","ze5","zf5","zg5","zh5","zi5"];
+let img5_3 = ["zj5","zk5","zl5","zm5","zn5","zo5","zp5","zq5","zr5","zs5"];
+
+function fun5_1(){
+    mainBox.innerHTML = "";
+    img5_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun5_2(){
+    mainBox.innerHTML = "";
+    img5_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun5_3(){
+    mainBox.innerHTML = "";
+    img5_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+
+let img6_1 = ["a6","b6","c6","d6","e6","f6","g6","h6"];
+
+function fun6_1(){
+    mainBox.innerHTML = "";
+    img6_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="./${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
